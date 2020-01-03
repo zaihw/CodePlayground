556253cdef4dd1fa6cb6ecbaff739b2bc24b260b
zaihw <zaihao_wang@outlook.com>
2020-01-03 12:11:56 -0800

    70 climbing stairs

---
+++ 70_climing_stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a[n];
+        a[0]=1;
+        a[1]=2;
+        
+        for (int i=2; i<n; ++i)
+        {
+            a[i]=a[i-1]+a[i-2];
+        }
+        
+        return a[n-1];
+    }
+};
