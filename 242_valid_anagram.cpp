180b4056c457f4ba8f7afed87ee68069907cc9ff
zaihw <zaihao_wang@outlook.com>
2020-01-15 20:59:21 -0800

    242

---
+++ 242_valid_anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())   return false;
+        //use a size=26 vector as hash table, initialize to be 0
+        vector<int> letters(26, 0);
+        for (auto item : s){
+            letters[item - 'a']++;
+        }
+        for (auto item : s){
+            letters[item - 'a']--;
+        }
+        for (auto letter : letters){
+            if (letter != 0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
