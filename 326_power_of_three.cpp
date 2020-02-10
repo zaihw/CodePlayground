50e9ec905f98bb2108ff767dd8dd58b008e63aad
zaihw <zaihao_wang@outlook.com>
2020-02-09 21:24:07 -0800

    326

---
+++ 326_power_of_three.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    //generate a map of all power of 3s within range (1,INT_MAX)
+    void init(unordered_map<int, bool>& power){
+        int p=1;
+        power[1]=true;
+        while(1){
+            p*=3;
+            power[p]=true;
+            if (p>INT_MAX/3)
+                break;
+        }
+    }
+public:
+    bool isPowerOfThree(int n) {
+        static unordered_map<int, bool> power;
+        if(power.size()==0) init(power);
+        //check if value 'n' presents in map 'power' by compare iterator woth power.end()
+        return power.find(n) != power.end();
+    }
+};
