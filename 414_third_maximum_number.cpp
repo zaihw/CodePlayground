ddb6720f5b83f40f3eba3ab812a488bdacd10637
zaihw <zaihao_wang@outlook.com>
2020-03-01 12:46:10 -0800

    414

---
+++ 414_third_maximum_number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nMax(vector<int>& nums, int n){
+        std::set<int> topN;
+        for(auto num : nums){
+            topN.insert(num);
+            if(topN.size()>n)
+                topN.erase(topN.begin());
+        }
+        return (topN.size()>=n) ? *(topN.begin()) : *(topN.rbegin());
+    }
+    
+    int thirdMax(vector<int>& nums) {
+        return nMax(nums, 3);
+    }
+};
