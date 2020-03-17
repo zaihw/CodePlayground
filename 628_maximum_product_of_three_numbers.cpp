1b4c9ae47a7c4460f8d553077cf8a2b202b69c26
zaihw <zaihao_wang@outlook.com>
2020-03-16 21:37:14 -0700

    628 typo

--- 628_maximum_product_of_three_numbers.cpp
+++ 628_maximum_product_of_three_numbers.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        //only two possiblities
+        //only two possibilities
         //1. two negative, one positive
-        //2. one negative, two positive
+        //2. three positive
         sort(nums.begin(), nums.end());
         int len=nums.size();
         int res=max(nums[0]*nums[1]*nums[len-1], nums[len-3]*nums[len-2]*nums[len-1]);
