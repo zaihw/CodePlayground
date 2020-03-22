36074ec64f0504d3a31db073f7bcea36b0107ff3
zaihw <30582036+zaihw@users.noreply.github.com>
2020-03-21 23:51:01 -0700

    724

--- 714_find_pivot_index.cpp
+++ 714_find_pivot_index.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        //1000 x 10000 < INT_MAX, so no overflow
+        //1000 x 10000 < INT_MAX, so no overflow need to check
         //loop thru nums and keep updating l_sum and r_sum from left to right
         //stop when l_sum = r_sum (only works when nums.size>=3)
         //size=0
